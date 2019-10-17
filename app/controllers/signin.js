317ce23b0be65656bdede03dbccdfa4ab862db80
Hugo Peixoto <hugo.peixoto@gmail.com>
2019-10-17 10:56:46 +0100

    Create auth service

--- app/controllers/signin.js
+++ app/controllers/signin.js
@@ -1,7 +1,6 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service'
-import authenticate from 'mob-web-ng/gql/mutations/authenticate';
 
 export default class SigninController extends Controller {
   @service
@@ -10,19 +9,17 @@ export default class SigninController extends Controller {
   @service
   router
 
+  @service
+  auth
+
   @action
   async submit(event) {
     event.preventDefault();
 
-    const response = await this.apollo.mutate({
-      mutation: authenticate,
-      variables: {
-        email: this.email,
-        password: this.password,
-      },
+    await this.auth.signin({
+      email: this.email,
+      password: this.password,
     });
-
-    localStorage.setItem("jwt", response.authenticate);
 
     this.router.transitionTo('welcome.you');
   }
--- app/services/apollo.js
+++ app/services/apollo.js
@@ -1,10 +1,14 @@
 import OriginalApolloService from 'ember-apollo-client/services/apollo';
 import { setContext } from 'apollo-link-context';
+import { inject as service } from '@ember/service'
 
 export default class ApolloService extends OriginalApolloService {
+  @service
+  auth
+
   link() {
     let httpLink = super.link(...arguments);
-    const authorizationHeader = localStorage.getItem("jwt") ? `Bearer ${localStorage.getItem("jwt")}` : "";
+    const authorizationHeader = this.auth.token() ? `Bearer ${this.auth.token()}` : "";
 
     let authLink = setContext(() => ({
       headers: { "Authorization": authorizationHeader },
